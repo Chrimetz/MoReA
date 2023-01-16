24450c62a937c603aa88841bc6d753ac97ae6bfa
Marcel Franzen <mfranzen@uni-bremen.de>
2023-01-16 15:26:32 +0100

    Add script to request api

---
+++ utils/request.py
@@ -0,0 +1,22 @@
+import requests
+
+API_URL = "http://moreapi.dsc.uni-bremen.de"
+API_PORT = 8000
+API_ROOT_PATH = "models"
+
+def get_all_models():
+    r = requests.get(url = API_URL + ":" + str(API_PORT) + "/" + API_ROOT_PATH)
+
+    return r.json()
+
+def get_model_details(model_name):
+    r = requests.get(url = API_URL + ":" + str(API_PORT) + "/" + API_ROOT_PATH + "/" + model_name)
+
+    return r.json()
+
+def request_model_inferencing(model_name, features):
+    data = {"features": features}
+
+    r = requests.post(url = API_URL + ":" + str(API_PORT) + "/" + API_ROOT_PATH + "/" + model_name, json=data)
+
+    return r.json()
